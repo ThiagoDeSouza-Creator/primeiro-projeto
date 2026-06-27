f608aa4e778d42af21cdcd0673ed92b46e35919f
Thiago De Souza Silva <thiago007002@gmail.com>
2026-06-26 21:27:52 -0300

    adicionando vetor

---
+++ atividades/vetores/lista_alunos.py
@@ -0,0 +1,7 @@
+import time
+
+alunos = ["Davi","Lucca","Debora","Iasmym","Marcos"]
+
+for aluno in alunos:
+    print(aluno)
+    time.sleep(1)
---
+++ atividades/vetores/vetor.py
@@ -0,0 +1,5 @@
+alunos =  ["Davi","Lucca","Wesley","Iasmym","Felipe","Romário","Thiago","DaviC","Weliton","Elisson","Vitória", "Samily","Julianny","Josias","Lorrany","Franciellen","WRaphael","Isabelle","Ana Clara","Alisson","João Elias"]
+alunos.sort()
+
+for i in range(len(alunos)):
+    print(f" {i+1}° Aluno(a) {alunos[i]}")
