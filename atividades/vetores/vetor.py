601e1f4ee4ba49fe94b90ba85fa91ac21570b302
Thiago De Souza Silva <thiago007002@gmail.com>
2026-06-30 21:16:08 -0300

    melhora na pasta vetores

--- atividades/vetores/vetor.py
+++ atividades/vetores/vetor.py
@@ -1,5 +1,20 @@
-alunos =  ["Davi","Lucca","Wesley","Iasmym","Felipe","Romário","Thiago","DaviC","Weliton","Elisson","Vitória", "Samily","Julianny","Josias","Lorrany","Franciellen","WRaphael","Isabelle","Ana Clara","Alisson","João Elias"]
+alunos =  ["Davi","Lucca","Wesley","Iasmym","Felipe","Romário","Thiago","DaviC","Weliton","Elisson","Vitória", "Samily","Julianny","Josias","Lorrany","Franciellen","WRaphael","Isabelle","Ana Clara","Alisson","João Elias","Thales"]
+
 alunos.sort()
 
 for i in range(len(alunos)):
     print(f" {i+1}° Aluno(a) {alunos[i]}")
+
+print("------------")
+print("usando for")
+print("------------")
+
+for aluno in alunos:
+    print(alunos)
+
+print("------------")
+print("usando while")
+print("------------")
+    
+conatdo = 0
+
